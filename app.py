4f700d16d9623e7b95cf58f46d52cadb02321fde
zaphod17 <40775649+zaphod17@users.noreply.github.com>
2018-07-09 23:36:25 +0200

    aggiunto pulsante luci

--- app.py
+++ app.py
@@ -23,12 +23,33 @@ GPIO.output(m12 , 0)
 GPIO.output(m21, 0)
 GPIO.output(m22, 0)
 GPIO.output(light, 0)
-print ("Pronto")
+
+# Create a dictionary called pins to store the pin number, name, and pin state:
+pins = {
+   8 : {'name' : 'GPIO 8', 'state' : GPIO.LOW}
+   }
+
+# Set each pin as an output and make it low:
+for pin in pins:
+   GPIO.setup(pin, GPIO.OUT)
+   GPIO.output(pin, GPIO.LOW)
+
+
+
+print ("Ready")
 
 a=1
 @app.route("/")
 def index():
-    return render_template('robot.html')
+   # For each pin, read the pin state and store it in the pins dictionary:
+   for pin in pins:
+      pins[pin]['state'] = GPIO.input(pin)
+   # Put the pin dictionary into the template data dictionary:
+   templateData = {
+      'pins' : pins
+      }
+   # Pass the template data into the template main.html and return it to the user
+    return render_template('robot.html', **templateData)
 
 @app.route('/left_side')
 def left_side():
@@ -87,6 +108,33 @@ def light_off():
    GPIO.output(light , 0)
    return  'true'
 
+# The function below is executed when someone requests a URL with the pin number and action in it:
+@app.route("/<changePin>/<action>")
+def action(changePin, action):
+   # Convert the pin from the URL into an integer:
+   changePin = int(changePin)
+   # Get the device name for the pin being changed:
+   deviceName = pins[changePin]['name']
+   # If the action part of the URL is "on," execute the code indented below:
+   if action == "on":
+      # Set the pin high:
+      GPIO.output(changePin, GPIO.HIGH)
+      # Save the status message to be passed into the template:
+      message = "Turned " + deviceName + " on."
+   if action == "off":
+      GPIO.output(changePin, GPIO.LOW)
+      message = "Turned " + deviceName + " off."
+
+   # For each pin, read the pin state and store it in the pins dictionary:
+   for pin in pins:
+      pins[pin]['state'] = GPIO.input(pin)
+
+   # Along with the pin dictionary, put the message into the template data dictionary:
+   templateData = {
+      'pins' : pins
+   }
+
+   return render_template('robot.html', **templateData)
 
 if __name__ == "__main__":
  print ("Start")
