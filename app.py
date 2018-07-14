e7b5744a8ee7683effb5a1f59d984b0c4ea020a5
zaphod17 <40775649+zaphod17@users.noreply.github.com>
2018-07-14 16:56:19 +0000

    Semplificazione del codice
    Tolti i cicli for sul dizionario pins e modificato il dizionario pins in Light

--- app.py
+++ app.py
@@ -5,90 +5,79 @@ import time
 
 app = Flask(__name__)
 
-m11=18
-m12=23
-m21=24
-m22=25
 
+#Define pins numbers of motor controll pins
+mRight=18
+mForward=23
+mLeft=24
+mBack=25
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(m11, GPIO.OUT)
-GPIO.setup(m12, GPIO.OUT)
-GPIO.setup(m21, GPIO.OUT)
-GPIO.setup(m22, GPIO.OUT)
-GPIO.output(m11 , 0)
-GPIO.output(m12 , 0)
-GPIO.output(m21, 0)
-GPIO.output(m22, 0)
+# Create a dictionary called Light to store the pin number, and pin state of LED lights:
+Light = {'pin' : 8 ,'state' : GPIO.LOW}
 
-
-# Create a dictionary called pins to store the pin number, name, and pin state:
-pins = {
-   8 : {'name' : 'Light', 'state' : GPIO.LOW}
-   }
 
 
 # Set each pin as an output and make it low:
-for pin in pins:
-   GPIO.setup(pin, GPIO.OUT)
-   GPIO.output(pin, GPIO.LOW)
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(mRight, GPIO.OUT)
+GPIO.setup(mForward, GPIO.OUT)
+GPIO.setup(mLeft, GPIO.OUT)
+GPIO.setup(mBack, GPIO.OUT)
+GPIO.output(mRight , 0)
+GPIO.output(mForward , 0)
+GPIO.output(mLeft, 0)
+GPIO.output(mBack, 0)
+GPIO.setup(Light['pin'], GPIO.OUT)
+GPIO.output(Light['pin'], GPIO.LOW)
 
 
 
 
 print ("Ready")
 
-a=1
 @app.route("/")
 def index():
-   # For each pin, read the pin state and store it in the pins dictionary:
-   for pin in pins:
-      pins[pin]['state'] = GPIO.input(pin)
-   # Put the pin dictionary into the template data dictionary:
+   # Read the Light state and store it in the Light dictionary:
+   Light['state'] = GPIO.input(Light['pin'])
+   # Put the Light dictionary into the template data dictionary:
    templateData = {
-      'pins' : pins
+      'Light' : Light
       }
    # Pass the template data into the template main.html and return it to the user
-   return render_template('robot.html', **templateData)
+   return render_template('main.html', **templateData)
 
-@app.route('/left_side')
+@app.route('/left')
 def left_side():
-    data1="LEFT"
-    GPIO.output(m21 , 1)
+    GPIO.output(mLeft , 1)
     return 'true'
 
-@app.route('/right_side')
+@app.route('/right')
 def right_side():
-   data1="RIGHT"
-   GPIO.output(m11 , 1)
+   GPIO.output(mRight , 1)
    return 'true'
 
-@app.route('/up_side')
+@app.route('/up')
 def up_side():
-   data1="FORWARD"
-   GPIO.output(m12 , 1)
+   GPIO.output(mForward , 1)
    return 'true'
 
-@app.route('/down_side')
+@app.route('/down')
 def down_side():
-   data1="BACK"
-   GPIO.output(m22 , 1)
+   GPIO.output(mBack , 1)
    return 'true'
 
-@app.route('/stopAD')
-def stopAD():
-   data1="STOPad"
-   GPIO.output(m12 , 0)
-   GPIO.output(m22 , 0)
+@app.route('/stopFB')
+def stopFB():
+   GPIO.output(mForward , 0)
+   GPIO.output(mBack , 0)
    return  'true'
 
 
-@app.route('/stopDS')
-def stopDS():
-   data1="STOPds"
-   GPIO.output(m11 , 0)
-   GPIO.output(m21 , 0)
+@app.route('/stopLR')
+def stopLR():
+   GPIO.output(mRight , 0)
+   GPIO.output(mLeft , 0)
    return  'true'
 
 
@@ -97,30 +86,29 @@ def stopDS():
 def action(changePin, action):
    # Convert the pin from the URL into an integer:
    changePin = int(changePin)
-   # Get the device name for the pin being changed:
-   deviceName = pins[changePin]['name']
+    
    # If the action part of the URL is "on," execute the code indented below:
    if action == "on":
       # Set the pin high:
       GPIO.output(changePin, GPIO.HIGH)
-      # Save the status message to be passed into the template:
-      message = "Turned " + deviceName + " on."
+      
    if action == "off":
       GPIO.output(changePin, GPIO.LOW)
-      message = "Turned " + deviceName + " off."
+      
 
-   # For each pin, read the pin state and store it in the pins dictionary:
-   for pin in pins:
-      pins[pin]['state'] = GPIO.input(pin)
+   # Read the Light state and store it in the Light dictionary:  
+   Light['state'] = GPIO.input(Light['pin'])
 
-   # Along with the pin dictionary, put the message into the template data dictionary:
+   # Pass the template data into the template main.html and return it to the user
    templateData = {
-      'pins' : pins
+      'Light' : Light
    }
 
-   return render_template('robot.html', **templateData)
+   return render_template('main.html', **templateData)
 
 
 if __name__ == "__main__":
  print ("Start")
  app.run(host='0.0.0.0',port=5010)
+
+
